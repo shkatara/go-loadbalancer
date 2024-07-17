f0f659a08025e31efe39b2e1da05cb5b6be09257
Shubham Katara <shubham.katara@trivago.com>
2024-07-17 19:37:49 +0200

    Round Robin Load Balancer

--- main.go
+++ main.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"io"
+	"math/rand"
+	"net/http"
+	"os"
 
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/client"
@@ -32,8 +37,19 @@ func listContainers() []string {
 
 func GetAllEvents(c *gin.Context) {
 	ipAddressList := listContainers()
+	min := 0
+	max := len(ipAddressList)
+	targetServer := rand.Intn(max-min) + min
+	requestURL := fmt.Sprintf("http://%s", ipAddressList[targetServer])
+	res, err := http.Get(requestURL)
+	if err != nil {
+		fmt.Printf("error making http request: %s\n", err)
+		os.Exit(1)
+	}
+	body, _ := io.ReadAll(res.Body)
 	c.JSON(200, gin.H{
-		"ContainersList": ipAddressList,
+		"containerIP": requestURL,
+		"data":        string(body),
 	})
 }
 
