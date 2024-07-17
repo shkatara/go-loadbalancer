2cdbf392281d79a02718e11c27b67d90d31c2db7
Shubham Katara <shubham.katara@trivago.com>
2024-07-17 19:13:38 +0200

    Return IP address of running nginx containers

--- main.go
+++ main.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"context"
+
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/client"
+	"github.com/gin-gonic/gin"
+)
+
+func listContainers() []string {
+	//var containerList = []string{}
+	var ipAddressList = []string{}
+
+	apiClient, err := client.NewClientWithOpts(client.FromEnv)
+	if err != nil {
+		panic(err)
+	}
+	defer apiClient.Close()
+	containers, err := apiClient.ContainerList(context.Background(), container.ListOptions{All: false})
+
+	if err != nil {
+		panic(err)
+	}
+	for _, ctr := range containers {
+		inspect, _ := apiClient.ContainerInspect(context.Background(), ctr.ID)
+		//fmt.Println(inspect.NetworkSettings.IPAddress)
+		ipAddressList = append(ipAddressList, inspect.NetworkSettings.IPAddress)
+	}
+	return ipAddressList
+}
+
+func GetAllEvents(c *gin.Context) {
+	ipAddressList := listContainers()
+	c.JSON(200, gin.H{
+		"ContainersList": ipAddressList,
+	})
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	// Export env var export DOCKER_API_VERSION=1.39
+	gin.SetMode(gin.ReleaseMode)
+	r := gin.Default()
+
+	r.GET("/", GetAllEvents)
+
+	r.Run(":8080")
 }
