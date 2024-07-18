f9e677a7c10b98e6a799fc3f7cf799d55ad5b443
Shubham Katara <shubham.katara@trivago.com>
2024-07-18 10:49:54 +0200

    Add Redirect

--- routes/routes.go
+++ routes/routes.go
@@ -2,10 +2,8 @@ package routes
 
 import (
 	"fmt"
-	"io"
 	"math/rand"
 	"net/http"
-	"os"
 
 	"github.com/gin-gonic/gin"
 	"trivago.com/shkatara/goLoadBalancer/controller"
@@ -17,14 +15,5 @@ func GetAllEvents(c *gin.Context) {
 	max := len(ipAddressList)
 	targetServer := rand.Intn(max-min) + min
 	requestURL := fmt.Sprintf("http://%s", ipAddressList[targetServer])
-	res, err := http.Get(requestURL)
-	if err != nil {
-		fmt.Printf("error making http request: %s\n", err)
-		os.Exit(1)
-	}
-	body, _ := io.ReadAll(res.Body)
-	c.JSON(200, gin.H{
-		"containerIP": requestURL,
-		"data":        string(body),
-	})
+	c.Redirect(http.StatusMovedPermanently, requestURL)
 }
